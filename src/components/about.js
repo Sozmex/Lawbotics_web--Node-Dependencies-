796959016072db3e34e216808d560b95271bbf1c
Sozmex <86256147+Sozmex@users.noreply.github.com>
2023-04-11 19:14:56 -0700

    first commit

---
+++ src/components/about.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+import Eyebrow from "./eyebrow";
+import AwardBadge from "../images/leaf.png";
+
+const About = () => {
+  const data = useStaticQuery(graphql`
+    {
+      aboutimage: file(relativePath: { eq: "about-law.png" }) {
+        childImageSharp {
+          gatsbyImageData(
+            width: 592
+            placeholder: BLURRED
+            formats: [AUTO, WEBP, AVIF]
+          )
+        }
+      }
+    }
+  `);
+  return (
+    <div id="#about">
+      <div className="container mx-auto">
+        <div className="grid lg:grid-cols-12 grid-cols-1 lg:gap-8 gap-20 lg:py-32 py-12 items-center">
+          <div className="lg:col-span-6 flex flex-col gap-6">
+            <Eyebrow label="ABOUT US" />
+            <h2 className="font-display md:text-display-xl text-display-md font-normal pb-4">
+             <span className="italic">AI Pioneers</span>{" "}
+             for Legal Excellence
+            </h2>
+            <p className="md:text-body-lg text-body-md font-light text-neutral-700">
+            By meticulously analyzing AI innovations, we handpick the most effective tools and strategies to optimize legal practices. Our mission is to streamline legal processes, enhance decision-making, and drive efficiency, enabling law firms to stay ahead in the competitive landscape and deliver exceptional client experiences.
+            </p>
+            <p className="md:text-body-lg text-body-md font-light text-neutral-700">
+            The emerging importance of AI in the legal industry cannot be overstated. Harnessing AI technology enables law firms to optimize processes, enhance efficiency, and improve decision-making. By automating repetitive tasks, streamlining research, and offering predictive analytics, AI solutions are transforming the practice of law, driving innovation, and empowering legal professionals to deliver superior results while focusing on high-value tasks and client relationships.
+            </p>
+          </div>
+          <div className="lg:col-span-6 flex flex-col gap-8 relative">
+            <GatsbyImage
+              image={getImage(data.aboutimage)}
+              alt="Interior Design"
+            />
+            <img
+              src={AwardBadge}
+              alt="Award Badge"
+              className="absolute left-[42%] -top-14 scale-75"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default About;
---
+++ src/components/metrics.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Metrics = () => {
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="flex flex-col">
+          <div className="grid md:grid-cols-12 grid-cols-1 gap-8">
+            <div className="md:col-span-4 flex xl:flex-row md:flex-col flex-row xl:gap-4 md:gap-0 gap-4 xl:items-center md:items-start items-center">
+              <p className="font-display md:text-display-xl text-display-lg font-normal">
+                100M
+              </p>
+              <p className="text-body-sm font-normal tracking-wider pt-2">
+                ChatGPT <br></br>USERS
+              </p>
+            </div>
+            <div className="md:col-span-4 flex xl:flex-row md:flex-col flex-row xl:gap-4 md:gap-0 gap-4 xl:items-center md:items-start items-center">
+              <p className="font-display md:text-display-xl text-display-lg font-normal">
+                1.591B
+              </p>
+              <p className="text-body-sm font-normal tracking-wider pt-2">
+                AI MARKET SIZE <br></br>BY 2030
+              </p>
+            </div>
+            <div className="md:col-span-4 flex xl:flex-row md:flex-col flex-row xl:gap-4 md:gap-0 gap-4 xl:items-center md:items-start items-center">
+              <p className="font-display md:text-display-xl text-display-lg font-normal">
+                38.1%
+              </p>
+              <p className="text-body-sm font-normal tracking-wider pt-2">
+                CAGR 2022-2030 <br></br>AI INDUSTRY
+              </p>
+            </div>
+          </div>
+          <div className="md:mt-20 md:pb-12 mt-12 pb-12">
+            <hr className="text-neutral-300"></hr>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Metrics;
