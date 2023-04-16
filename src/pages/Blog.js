3aeb6bbc5b270c882cb929b4934e7ed7150d8594
Sozmex <zachmex1@gmail.com>
2023-04-16 19:29:19 -0400

    fix read more button attempt 2

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 
+import Seo from "../components/seo";
 import Layout from "../components/layout";
+import Hero from "../components/hero";
+import Metrics from "../components/metrics";
+import Services from "../components/services";
+import Awards from "../components/awards";
+import About from "../components/about";
 import Works from "../components/works";
+import Works1 from "../components/works1";
 
 const BlogPage = () => (
   <Layout>
