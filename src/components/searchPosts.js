34b29fb2215819a38dcb44fa5bcd6881eaa00a52
Sozmex <86256147+Sozmex@users.noreply.github.com>
2023-04-21 16:51:36 -0400

    changes to formatting, removed render by the srch

--- src/components/searchPosts.js
+++ src/components/searchPosts.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react"
-import { Link } from "gatsby"
-import styled from "styled-components"
-import { useFlexSearch } from "react-use-flexsearch"
-import * as queryString from "query-string"
+import React, { useState } from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+import { useFlexSearch } from "react-use-flexsearch";
+import * as queryString from "query-string";
 
-import { rhythm } from "../utils/typography"
+import { rhythm } from "../utils/typography";
 
 const SearchBar = styled.div`
   display: flex;
@@ -17,8 +17,8 @@ const SearchBar = styled.div`
 
   svg {
     margin: auto 1rem;
-    height: 20px;
-    width: 20px;
+    height: 16px;
+    width: 16px;
     color: #9aa0a6;
     fill: #9aa0a6;
   }
@@ -39,16 +39,16 @@ const SearchBar = styled.div`
     word-wrap: break-word;
     outline: none;
   }
-`
+`;
 
 const SearchedPosts = ({ results }) =>
   results.length > 0 ? (
     results.map(node => {
-      const date = node.date
-      const title = node.title || node.slug
-      const description = node.description
-      const excerpt = node.excerpt
-      const slug = node.slug
+      const date = node.date;
+      const title = node.title || node.slug;
+      const description = node.description;
+      const excerpt = node.excerpt;
+      const slug = node.slug;
 
       return (
         <div key={slug}>
@@ -68,77 +68,52 @@ const SearchedPosts = ({ results }) =>
             }}
           />
         </div>
-      )
+      );
     })
   ) : (
     <p style={{ textAlign: "center" }}>
       Sorry, couldn't find any posts matching this search.
     </p>
-  )
-
-const AllPosts = ({ posts }) => (
-  <div style={{ margin: "20px 0 40px" }}>
-    {posts.map(({ node }) => {
-      const title = node.frontmatter.title || node.fields.slug
-      return (
-        <div key={node.fields.slug}>
-          <h3
-            style={{
-              marginBottom: rhythm(1 / 4),
-            }}
-          >
-            <Link style={{ boxShadow: `none` }} to={`/blog${node.fields.slug}`}>
-              {title}
-            </Link>
-          </h3>
-          <small>{node.frontmatter.date}</small>
-          <p
-            dangerouslySetInnerHTML={{
-              __html: node.frontmatter.description || node.excerpt,
-            }}
-          />
-        </div>
-      )
-    })}
-  </div>
-)
+  );
 
 const SearchPosts = ({ posts, localSearchBlog, location, navigate }) => {
-  const { search } = queryString.parse(location.search)
-  const [query, setQuery] = useState(search || "")
+  const { search } = queryString.parse(location.search);
+  const [query, setQuery] = useState(search || "");
+  const [submittedQuery, setSubmittedQuery] = useState(search || "");
 
   const results = useFlexSearch(
-    query,
+    submittedQuery,
     localSearchBlog.index,
     JSON.parse(localSearchBlog.store)
-  )
+  );
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    navigate(submittedQuery ? `/blog/?search=${submittedQuery}` : "/blog/");
+  };
 
   return (
     <>
       <SearchBar>
-        <svg
-          focusable="false"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-        </svg>
-        <input
-          id="search"
-          type="search"
-          placeholder="Search all posts"
-          value={query}
-          onChange={e => {
-            navigate(
-              e.target.value ? `/blog/?search=${e.target.value}` : "/blog/"
-            )
-            setQuery(e.target.value)
-          }}
-        />
+        <form onSubmit={handleSearchSubmit}>
+          <svg
+            onClick={handleSearchSubmit}
+            focusable="false"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+          </svg>
+          <input
+            id="search"
+            type="search"
+            placeholder="Search all posts"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
+        </form>
       </SearchBar>
-      {query ? <SearchedPosts results={results} /> : <AllPosts posts={posts} />}
+      {submittedQuery ? <SearchedPosts results={results} /> : <div></div>}
     </>
-  )
+  );
 }
-
-export default SearchPosts
--- src/pages/Blog2.js
+++ src/pages/Blog2.js
@@ -1,26 +1,26 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+// Blog2.js
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Button from "../components/button"
-import SearchPosts from "../components/searchPosts"
-import * as blogstyle from '../styles/blogStyles.module.css'
+import Bio from "../components/bio";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Button from "../components/button";
+import SearchPosts from "../components/searchPosts";
+import * as blogstyle from "../styles/blogStyles.module.css";
 
 class Blog2 extends React.Component {
   render() {
-    const { data, navigate, location } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMdx.edges
-    const localSearchBlog = data.localSearchBlog
-
-    // ... rest of the code
+    const { data, navigate, location } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
+    const posts = data.allMdx.edges;
+    const localSearchBlog = data.localSearchBlog;
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
         <div className={blogstyle.container}>
+          <h1 className={blogstyle.title}>Blog</h1>
           <SearchPosts
             posts={posts}
             localSearchBlog={localSearchBlog}
@@ -28,18 +28,36 @@ class Blog2 extends React.Component {
             location={location}
           />
           <div className={blogstyle.postItem}>
-            {/* Your post list code */}
+            {posts.map(({ node }) => {
+              const title = node.frontmatter.title || node.fields.slug;
+              return (
+                <div key={node.fields.slug}>
+                  <h2 className={blogstyle.postTitle}>
+                    <Link to={node.fields.slug}>{title}</Link>
+                  </h2>
+                  <small className={blogstyle.postDate}>
+                    {node.frontmatter.date}
+                  </small>
+                  <p
+                    className={blogstyle.excerpt}
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                  />
+                </div>
+              );
+            })}
           </div>
           <Link className={blogstyle.goHomeButton} to="/">
             <Button marginTop="0">Go Home</Button>
           </Link>
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default Blog2
+export default Blog2;
 
 export const pageQuery = graphql`
   query {
@@ -68,4 +86,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
