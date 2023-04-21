8602a6f39c6568c4a3639e2791b302a0cc094e3b
Sozmex <86256147+Sozmex@users.noreply.github.com>
2023-04-21 19:01:31 -0400

    fixed blog database

--- src/pages/Blog2.js
+++ src/pages/Blog2.js
@@ -1,63 +1,37 @@
-// Blog2.js
-import React from "react";
-import { Link, graphql } from "gatsby";
+import React from "react"
+import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio";
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import Button from "../components/button";
-import SearchPosts from "../components/searchPosts";
-import * as blogstyle from "../styles/blogStyles.module.css";
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Button from "../components/button"
+import SearchPosts from "../components/searchPosts"
 
-class Blog2 extends React.Component {
+class Blog extends React.Component {
   render() {
-    const { data, navigate, location } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
-    const posts = data.allMdx.edges;
-    const localSearchBlog = data.localSearchBlog;
+    const { data, navigate, location } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMdx.edges
+    const localSearchBlog = data.localSearchBlog
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
-        <div className={blogstyle.container}>
-          <h1 className={blogstyle.title}>Blog</h1>
-          <SearchPosts
-            posts={posts}
-            localSearchBlog={localSearchBlog}
-            navigate={navigate}
-            location={location}
-          />
-          <div className={blogstyle.postItem}>
-            {posts.map(({ node }) => {
-              const title = node.frontmatter.title || node.fields.slug;
-              return (
-                <div key={node.fields.slug}>
-                  <h2 className={blogstyle.postTitle}>
-                    <Link to={node.fields.slug}>{title}</Link>
-                  </h2>
-                  <small className={blogstyle.postDate}>
-                    {node.frontmatter.date}
-                  </small>
-                  <p
-                    className={blogstyle.excerpt}
-                    dangerouslySetInnerHTML={{
-                      __html: node.frontmatter.description || node.excerpt,
-                    }}
-                  />
-                </div>
-              );
-            })}
-          </div>
-          <Link className={blogstyle.goHomeButton} to="/">
-            <Button marginTop="0">Go Home</Button>
-          </Link>
-        </div>
+        <SearchPosts
+          posts={posts}
+          localSearchBlog={localSearchBlog}
+          navigate={navigate}
+          location={location}
+        />
+        <Link to="/">
+          <Button marginTop="85px">Go Home</Button>
+        </Link>
       </Layout>
-    );
+    )
   }
 }
 
-export default Blog2;
+export default Blog
 
 export const pageQuery = graphql`
   query {
@@ -86,4 +60,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
